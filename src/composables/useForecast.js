c5eeeb40f740ea71c48c269f4dd907bb15cda71b
Stanislav <43533425+Soft-Scale@users.noreply.github.com>
2023-12-28 15:43:47 +0200

    replace http to https:

--- src/composables/useForecast.js
+++ src/composables/useForecast.js
@@ -1,7 +1,7 @@
 export function useForecast() {
   const apiKey = "531fd3fdf9484783a4590543232610"
   const getForecast = async (cityName) => {
-    const apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&aqi=nor&days=5&lang=en`
+    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&aqi=nor&days=5&lang=en`
     const response = await fetch(apiUrl)
 
     return  await response.json()
